0ee1adcb138de76e6d0e3d00ddbe1fc75b709344
Kevin Langat <kevinlangat419@gmail.com>
2026-03-14 23:35:57 +0300

    Started the prometheus and grafana for monitoring and metrics and finished dockerizing the app

--- PART THREE/2. Prisma Concepts/server.js
+++ PART THREE/2. Prisma Concepts/server.js
@@ -2,6 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const authorRoutes = require('./routes/author-routes');
 const bookRoutes = require('./routes/book-routes');
+const promClient = require('prom-client');
+
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics(register);
+
+const httpRequestsCounter = new promClient.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status'],
+});
 
 const app = express();
 app.use(express.json());
